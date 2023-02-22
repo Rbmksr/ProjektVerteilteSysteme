4363c313418b99df5482fde4ef0999dafc2a4263
Benwegel <115665601+Benwegel@users.noreply.github.com>
2023-02-22 12:55:21 +0100

    ExampleShoe in Men und Women

--- controllers/shoe2Women.js
+++ controllers/shoe2Women.js
@@ -1,46 +1,45 @@
 import { check, validationResult } from "express-validator";
 
 const shoesWomen = [
-    {
-        id: 0,
-        title: "Project Hail Mary",
-        author: "Andy Weir",
-        pages: 496,
-    },
-    {
-        id: 1,
-        title: "Harrow the Ninth",
-        author: "Tamsyn Muir",
-        pages: 512,
-    },
+  {
+    id: 0,
+    name: "ExampleShoe",
+    category: "Sneaker",
+    brand: "Nike",
+    price: "150",
+    size: "43",
+    color: "black",
+  },
 ];
 
 export const getShoesWomen = (req, res) => {
-    res.status(200).send(shoesWomen);
+  res.status(200).send(shoesWomen);
 };
 
 export const findShoesWomen = (req, res) => {
-    let result = shoesWomen.filter((shoeWomen) => shoeWomen.title == req.query.title);
-    res.status(200).send(result);
+  let result = shoesWomen.filter(
+    (shoeWomen) => shoeWomen.title == req.query.title
+  );
+  res.status(200).send(result);
 };
 
 export const findShoesWomenById = (req, res) => {
-    let shoeWomen = shoesWomen.find((b) => b.id == req.params.id);
-    res.status(200).send(shoeWomen);
+  let shoeWomen = shoesWomen.find((b) => b.id == req.params.id);
+  res.status(200).send(shoeWomen);
 };
 
 export const addShoeWomen = (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    let shoeWomen = req.body;
-    shoesWomen.push(shoeWomen);
-    res.status(201).send(`Added ${shoeWomen.title} to book collection`);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  let shoeWomen = req.body;
+  shoesWomen.push(shoeWomen);
+  res.status(201).send(`Added ${shoeWomen.title} to book collection`);
 };
 
 // attached as second param in a route
 export const newShoeWomenValidators = [
-    check("title").notEmpty().withMessage("Title field required"),
-    check("author").notEmpty().withMessage("Author field required"),
+  check("title").notEmpty().withMessage("Title field required"),
+  check("author").notEmpty().withMessage("Author field required"),
 ];
--- controllers/shoe3Children.js
+++ controllers/shoe3Children.js
@@ -1,46 +1,45 @@
 import { check, validationResult } from "express-validator";
 
 const shoesChildren = [
-    {
-        id: 0,
-        title: "Project Hail Mary",
-        author: "Andy Weir",
-        pages: 496,
-    },
-    {
-        id: 1,
-        title: "Harrow the Ninth",
-        author: "Tamsyn Muir",
-        pages: 512,
-    },
+  {
+    id: 0,
+    name: "ExampleShoe",
+    category: "Sneaker",
+    brand: "Nike",
+    price: "150",
+    size: "43",
+    color: "black",
+  },
 ];
 
 export const getShoesChildren = (req, res) => {
-    res.status(200).send(shoesChildren);
+  res.status(200).send(shoesChildren);
 };
 
 export const findShoesChildren = (req, res) => {
-    let result = shoesChildren.filter((shoeChildren) => shoeChildren.title == req.query.title);
-    res.status(200).send(result);
+  let result = shoesChildren.filter(
+    (shoeChildren) => shoeChildren.title == req.query.title
+  );
+  res.status(200).send(result);
 };
 
 export const findShoesChildrenById = (req, res) => {
-    let shoeChildren = shoesChildren.find((b) => b.id == req.params.id);
-    res.status(200).send(shoeChildren);
+  let shoeChildren = shoesChildren.find((b) => b.id == req.params.id);
+  res.status(200).send(shoeChildren);
 };
 
 export const addShoeChildren = (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    let shoeChildren = req.body;
-    shoesChildren.push(shoeChildren);
-    res.status(201).send(`Added ${shoeChildren.title} to book collection`);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  let shoeChildren = req.body;
+  shoesChildren.push(shoeChildren);
+  res.status(201).send(`Added ${shoeChildren.title} to book collection`);
 };
 
 // attached as second param in a route
 export const newShoeChildrenValidators = [
-    check("title").notEmpty().withMessage("Title field required"),
-    check("author").notEmpty().withMessage("Author field required"),
+  check("title").notEmpty().withMessage("Title field required"),
+  check("author").notEmpty().withMessage("Author field required"),
 ];
