8f74a5b00d6cee615dcfe35e0d48d51330a52bf4
Rbmksr <xingo.op01@gmail.com>
2023-03-20 10:14:36 +0100

    change one wrong word

--- frontend/src/RestClient.js
+++ frontend/src/RestClient.js
@@ -1,6 +1,6 @@
 /* fetches api data from backend and extracts its json body */
-export async function fetchAllShoes() {
-  return await fetch("http://localhost:4000/", {
+export async function fetchAllShoeMen() {
+  return await fetch("http://localhost:4000/shoeMen", {
     mode: "cors",
   })
     .then((res) => {
