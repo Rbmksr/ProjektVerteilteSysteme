622abe29c31a361caf595fc773d6843d7d1dc8f7
Benwegel <115665601+Benwegel@users.noreply.github.com>
2023-03-20 10:03:09 +0100

    Frontend angleichung

--- frontend/src/RestClient.js
+++ frontend/src/RestClient.js
@@ -1,12 +1,12 @@
 /* fetches api data from backend and extracts its json body */
-export async function fetchAllBooks() {
-    return await fetch("http://localhost:4000/", {
-      mode: "cors",
+export async function fetchAllShoes() {
+  return await fetch("http://localhost:4000/", {
+    mode: "cors",
+  })
+    .then((res) => {
+      return res.json();
     })
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        return res;
-      });
-  }
+    .then((res) => {
+      return res;
+    });
+}
