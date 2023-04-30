d2aa688ba5b6a06a371b1e8f66e1436e1e5e3ef7
fmzk84 <stoffe05balsa@icloud.com>
2023-04-30 21:11:42 +0200

    final fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import shoeChildrenRouter from "./routes/shoeChildrenRoutes.js";
 
 // app initialisieren
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
